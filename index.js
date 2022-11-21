4bbaa9ae41cc7486482a07d1420e17373a7b4e66
pauline-njunguna <paulinenjunguna.w@gmail.com>
2022-11-21 15:08:55 +0300

    Done

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
 // Write your code in this file!
+ const currentUser = "Grace Hopper";
+{
+    let welcomeMessage="Welcome to Flatbook, Grace Hopper!"
+    let welcometoflatbook = "Welcome to Flatbook,";
+    const currentUser = "Grace Hopper";
+}
+{
+    let excitedWelcomeMessage = "WELCOME TO FLATBOOK, GRACE HOPPER!";
+    let welcometoflatbook = "WELCOME TO FLATBOOK, ";
+    const currentUser = "GRACE HOPPER";
+}
+{
+    let shortGreeting="Welcome, G. Hopper!";
+     let welcome = "Welcome ";
+     const currentUser = "G. HOPPER";
+
+}
